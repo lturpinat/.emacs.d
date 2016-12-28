958133b89254f427d7b306311db0b8bab5ea63a2
lturpinat <turpinat.louis@gmail.com>
2016-12-28 15:18:53 +0100

    Numerous adds and fixes...
    *Packages*
    
    Concerning company :
    - New backgrounds : company-irony (for C/C++ completion),
    company-c-headers, company-quickhelp (display help with company),
    company-statistics
    
    - Correctly mapped "tab" (works with yasnippet [plus displayed inside
      of selection])
    
    Whichkey : display available commands as shortcuts are entered
    
    Goldenratio : automatically resize buffers depending on the size that each
    one needs.
    
    Concerning Latex :
    - latex-extra, latex-math-preview and latex-unicode-math-code improve
    experience with org-mode.
    
    - It is now possible de generate LateX images in org-mode thanks to
      imagemagick
    
    Concerning Helm :
    - helm-find (search recursivelly from current folder --> "C-x f"),
    helm-swoop (interactive search in buffer --> "C-c s"),
    helm-show-kill-ring ("M-y") are binded.
    
    - "tab" now open folders instead of displaying help
    
    Concerning org-mode :
    - Source blocks are now nativelly syntaxically colored
    - The latex formulas are now generated with imagemagick
    
    Concerning irony :
    Irony is fulling configured (libclang is not to be forgotten, also
    remember to do irony-install-server one time)
    
    Concerning smart-parens : hightlights separators like "[", "{"...
    
    *Configuration & Hacks*
    
    - the emacs startup screen is now disabled
    - "package-refresh-contents" is called when starting-up
    - my name is set (through the variable "user-full-name")
    - the default identation is now set to two spaces
    - keywords like "TODO:", "FIXME:" are highlighted in "prog-mode"
    - kill-buffer with "C-x k" is fixed with a function calling
    "kill-buffer"
    - read-only-mode is remapped : "C-x C-g"
    - The start-up time is displayed when emacs is loaded

--- init.el
+++ init.el
@@ -6,18 +6,14 @@
 
 (require 'org) 
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "settings.org")))
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("d5b121d69e48e0f2a84c8e4580f0ba230423391a78fcb4001ccb35d02494d79e" default)))
  '(package-selected-packages
    (quote
-    (org-bullets blackboard-theme smart-tab company-irony-c-headers zenburn-theme yasnippet yaml-mode wttrin window-numbering web-mode wc-mode syslog-mode spaceline solarized-theme smartparens rainbow-delimiters python-mode popwin pdf-tools ox-reveal multiple-cursors minimap markdown-mode magit iedit htmlize hlinum highlight helm-swoop helm-smex helm-ls-git helm-fuzzier helm-flx helm-dash focus flycheck-irony fix-word expand-region direx dash-at-point csharp-mode company-irony calfw bm beacon anzu ace-jump-mode))))
+    (company-quickhelp pos-tip golden-ratio zenburn-theme yasnippet yaml-mode wttrin window-numbering which-key web-mode wcheck-mode wc-mode syslog-mode spaceline solarized-theme smartparens rainbow-delimiters python-mode popwin pdf-tools ox-reveal org-bullets names multiple-cursors minimap markdown-mode magit latex-unicode-math-mode latex-math-preview latex-extra iedit htmlize hlinum highlight helm-swoop helm-smex helm-ls-git helm-fuzzier helm-flx helm-descbinds helm-dash gnuplot-mode gnuplot focus flycheck-irony fix-word expand-region direx dash-at-point csharp-mode company-irony-c-headers company-irony calfw bm blackboard-theme beacon anzu ace-jump-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
