bb33692bc1f7b464f65303b3f1ff6032f7854704
lturpinat <turpinat.louis@gmail.com>
2017-02-04 00:58:50 +0100

    Ajout de bug-hunter
    bug-hunter permet de tracker les erreurs d'un init-file

--- init.el
+++ init.el
@@ -5,18 +5,3 @@
 (require 'org) 
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "settings.org")))
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-agenda-files (quote ("~/todo.org")))
- '(package-selected-packages
-   (quote
-    (org-pomodoro systemd ppd-sr-speedbar projectile javadoc-lookup zenburn-theme yasnippet yaml-mode wttrin window-numbering which-key web-mode wc-mode undo-tree syslog-mode spotify spaceline solarized-theme smartparens slime-company rainbow-delimiters python-mode popwin pdf-tools ox-reveal org-bullets multiple-cursors minimap markdown-mode magit latex-unicode-math-mode latex-math-preview latex-extra iedit htmlize hlinum highlight-numbers highlight helm-swoop helm-smex helm-ls-git helm-fuzzier helm-flx helm-dash golden-ratio focus flycheck-irony fix-word expand-region direx dash-at-point csharp-mode company-statistics company-quickhelp company-irony company-c-headers color-theme-sanityinc-tomorrow calfw bug-hunter bm blackboard-theme beacon anzu ace-jump-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
